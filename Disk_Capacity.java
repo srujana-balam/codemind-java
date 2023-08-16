476154cf260a46e6cb0a6a444942655527b7c3cb
srujana <119722973+srujana-balam@users.noreply.github.com>
2023-08-16 14:26:31 +0530

    import java.util.Scanner; public class Division{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int a,b,c;         a=sc.nextInt();         b=sc.nextInt();         c=sc.nextInt();         System.out.println(2*a*b*c*512);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Division{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a,b,c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        System.out.println(2*a*b*c*512);
+    }
+}
