1c99104b760a186c17c1a0adc737a8924a1cf1d2
srujana <119722973+srujana-balam@users.noreply.github.com>
2023-09-07 10:11:03 +0530

    import java.util.*; public class AverageWeight{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int x,a,b,c;         x=sc.nextInt();         a=sc.nextInt();         b=sc.nextInt();         c=3*x-(a+b);         System.out.println(c);     } }

---
+++ Average_weight.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class AverageWeight{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x,a,b,c;
+        x=sc.nextInt();
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=3*x-(a+b);
+        System.out.println(c);
+    }
+}
